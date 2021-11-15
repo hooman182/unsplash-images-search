59402278bc453d6deb50db52cba61b45d3a02d25
hooman <hoomanm87@gmail.com>
2021-11-15 23:04:48 +0330

    refactor: Change css styles in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header className="bg-gray-200 flex justify-between items-center px-5">
             <img src={imageIcon} alt="image icon" className="" />
-            <form method="post" className="flex-grow mx-4">
+            <form method="post" className="flex-grow mx-6">
                 <input type="text" placeholder="Search in unsplash..." className="w-full outline-none p-2.5" />
             </form>
         </header>
